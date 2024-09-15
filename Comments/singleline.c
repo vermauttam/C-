44cbfb662488c7024f9e71eed6c4f2fc5c58219f
vermauttam <contacts2uttam@gmail.com>
2024-09-15 13:18:26 +0530

    Variables and Comments added

---
+++ Comments/singleline.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 20;
+    // int b = 30;             This is a single line comment in C
+    printf("%d",a);
+    // printf("%d",b);         This will not be executed
+
+    
+
+}
---
+++ variables/sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int num1 = 20, num2 = 20;
+    int result = num1 + num2;
+    printf("num1 + num2 is :%d",result);
+    return 0;
+}
