da87510dcc6ddddeab47f523f8e0d683b53353be
vermauttam <contacts2uttam@gmail.com>
2024-09-15 13:30:35 +0530

    Multiline comment

---
+++ Comments/multiline.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+/*
+
+int a = 20;
+printf("%d",a);                      This will not be executed as it is under multiline comment
+
+
+*/
+int b = 30;
+printf("%d",b);                    // This will be executed 
+
+}
