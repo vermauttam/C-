2e594fcc4869bc6c007da522c8b6fe5389487254
vermauttam <contacts2uttam@gmail.com>
2024-10-05 23:52:37 +0530

    Statements in C

--- statements/controlStatements.c
+++ statements/controlStatements.c
@@ -122,3 +122,319 @@ int main() {
 }
 
 */
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a =10;
+    if(a)
+    printf("Good");                                     output:GoodBad
+    printf("Bad");
+    return 0;
+}
+
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a = 0, b =1;
+    if (a,b)
+    {
+    printf("Good");                          //output: GoodBetter
+    printf("Better");
+
+    }
+    printf("Bad");
+    return 0;
+}
+
+*/
+
+
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    if(printf("%d",a))                          output:0GoodBad
+      printf("Good");
+    printf("Bad");
+    return 0;
+}
+
+*/
+
+
+/*
+
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    a = 1,0;
+    b = (1,0);
+    c = (0,1);
+    if(a)
+    {
+        printf("%d",a);                        output:1
+        if(b)
+        {
+          printf("%d",b);
+          if(c)
+          printf("%d",c);
+        }
+    }
+
+    return 0;
+}
+
+*/ 
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a = -1, b = 0, c = 1;
+    if(++a, b++, c--)
+        printf("HAI");
+    else if(a++, --b, ++c)
+        printf("HELLO");
+    else if(--a, c++, b++)                      output:HAI
+        printf("BYE");
+    return 0;
+}
+*/
+
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a =1, b = -1, c =0;
+    if(a,c,b)
+     printf("HAI");
+    if(++a,--c,b++)
+     printf("HELLO");                           output:HAIHELLOBYE
+    if(--b,c++,a) 
+     printf("BYE");
+    return 0;
+}
+
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a = 1, b = 0;
+    if(a,b)
+     printf("HAI");
+     printf("HELLO");                           output:'else' without a previous if
+    else
+     printf("BYE");
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    if(printf("%d",a--))
+     printf("%d",a);
+    if(printf(""))
+     printf("%d",a++);
+    else                                                output:10-1
+     printf("%d",--a);
+     return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {
+
+    }
+    return 0;
+}
+*/
+
+
+//Nested if
+/*
+if(exp1)
+{
+    if(exp2)
+    {
+
+    }
+}
+
+*/
+
+
+//Nested if-else
+/*
+if(Exp)
+{
+    if(Exp)
+    {
+        
+    }
+    else
+    {
+
+    }
+}
+else
+{
+    if(Exp)
+    {
+
+    }
+    else
+    {
+
+    }
+}
+*/
+
+
+//Else if-else ladder
+/*
+#include <stdio.h>
+
+int main() {
+    if(Exp)
+    {
+        stats;
+    }
+    else if(Exp)
+    {
+        Stats;
+    }
+    else if(Exp)
+    {
+        Stats;
+    }
+    else if(Exp)
+    {
+        Stats;
+    }
+    else
+    {
+        Stats;
+    }
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    if(scanf("%d%d%d",&a,&b,&c))
+     printf("Hai");
+    if(printf("%d%d%d",a,b,c))
+     printf("Hello");                                   //output: Hai5-7-9Hello
+    return 0;
+}
+
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int i = 5;
+    do{
+        printf("%d\n",i);                      // output:5
+        i=i+1;
+    }while(i<=5);
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    while(a<=5){                                     // output: infinte loop
+        printf("%d",a);
+    }
+    return 0;
+}
+
+*/
+
+
+/*
+#include <stdio.h>
+
+int main() {
+    int i = 3, j = 7;
+    do
+    {
+        printf("%d%d\n",i,j);                       //output:infinite loop
+        i = i + 1;
+        j = j - 2;
+    }while(i,j);
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int i = 5, j = 9, count = 0;
+    
+    do
+    {
+        printf("Hai\n");
+        count += 1;
+    }while(i--,j--);
+    printf("count = %d",count);
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int i = 1, j = 5;
+    do
+    {
+        printf("%d, ",j);
+        do
+        {
+            printf("%d, ",i);                          //output:5111-14-37
+            j = j - 2;
+        }while(j>=0);
+        i = i + 3;
+    }while(i<=9);
+    return 0;
+}
+
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    FILE * new_file;
+    new_file = fopen("uttam.txt","w");
+    return 0;
+}
